412d4ce0e1bdb29b58165c89ddff0cdb30598382
rgudivada21 <rgudivada21@gmail.com>
2025-11-10 19:18:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
 provider "aws" {
-  region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  count                  = 4
-  ami                    = "ami-02b8269d5e85954ef"
-  instance_type          = "t2.medium"
-  key_name               = "raj1"
-  vpc_security_group_ids = ["sg-0d6ffca9a1ad80e65"]
-  tags = {
-    Name = var.instance_names[count.index]
-  }
+count = 4
+ami = "ami-04aa00acb1165b32a"
+instance_type = "t2.medium"
+key_name = "argocd"
+vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+tags = {
+Name = var.instance_names[count.index]
 }
-
-variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
