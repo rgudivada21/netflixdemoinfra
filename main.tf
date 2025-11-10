1fafb37b37f194dcf154c04b1c8d93265ad66fab
rgudivada21 <rgudivada21@gmail.com>
2025-11-10 19:13:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-02b8269d5e85954ef"
   instance_type          = "t2.medium"
-  key_name               = "Netflix"
+  key_name               = "raj1"
   vpc_security_group_ids = ["sg-0d6ffca9a1ad80e65"]
   tags = {
     Name = var.instance_names[count.index]
